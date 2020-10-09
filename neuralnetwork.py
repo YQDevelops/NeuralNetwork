a29a5161d85b5302af324303b3acb5bad0e6892a
YQDevelops <lowyewqing@gmail.com>
2020-10-09 17:11:46 +0800

    Made changes to better fit PEP8

--- neuralnetwork.py
+++ neuralnetwork.py
@@ -3,19 +3,22 @@
 #       By:@YQDevelops
 #Objective:To Create A Simple Neural Network That Can Recognise Handwritten Digits
 #    Edits:29/3/2020-- Tried to do the cost function.
-#           1/4/2020-- Added way to find out accuracy, attempted the cost function
-#           3/4/2020-- Added the cost function, ran it but took to long to compute. Computer was overheating.
-#           9/4/2020-- Changed to Hadamard Product instead of matmul
+#          1/4/2020-- Added way to find out accuracy, attempted the cost function
+#          3/4/2020-- Added the cost function, ran it but took to long to compute. Computer was overheating.
+#          9/4/2020-- Changed to Hadamard Product instead of matmul
+#         10/9/2020-- Made changes in accordance with PEP8
 #
 ################################################################################################################################
+
+#Standard Library
 import numpy as np
 
 class ArtificialBrain:
 
-    def __init__(self, layerSizes):
-        weightShapes = [(a,b) for a,b in zip(layerSizes[1:], layerSizes[:-1])] #zip(from 2nd item until last),(from 1st item until 2nd last)
-        self.weights = [np.random.standard_normal(s)/layerSizes[0]**0.5 for s in weightShapes] #random.standard_normal() is randoms in the standard random deviation
-        self.biases = [np.zeros((s,1)) for s in layerSizes[1:]]
+    def __init__(self, layer_sizes):
+        weightShapes = [(a,b) for a,b in zip(layer_sizes[1:], layer_sizes[:-1])] #zip(from 2nd item until last),(from 1st item until 2nd last)
+        self.weights = [np.random.standard_normal(s)/layer_sizes[0]**0.5 for s in weightShapes] #random.standard_normal() is randoms in the standard random deviation
+        self.biases = [np.zeros((s,1)) for s in layer_sizes[1:]]
 
     def predict(self, input):
         for weights, biases in zip(self.weights, self.biases):
@@ -24,32 +27,31 @@ class ArtificialBrain:
 
         return input
 
-    def printAccuracy(self, images, labels):
+    def print_accuracy(self, images, labels):
         predictions = self.predict(images)
-        numCorrect = sum([np.argmax(a) == np.argmax(b) for a, b in zip(predictions, labels)])
+        num_correct = sum([np.argmax(a) == np.argmax(b) for a, b in zip(predictions, labels)])
 
-        print(f"{numCorrect}/{len(images)} accuracy: {(numCorrect/len(images)) * 100}%")
-        print("HelloWorld!")
+        print(f"{num_correct}/{len(images)} accuracy: {(num_correct/len(images)) * 100}%")
 
-    def getCost(self,input,trueLabels):
-        numberOfRuns = 0
+    def get_cost(self,input,true_labels):
+        num_of_runs = 0
         cost = 0
         for i in range(len(input)):
 
-            cost += self.cost(input,trueLabels,numberOfRuns)
-            numberOfRuns += 1
+            cost += self.cost(input,true_labels,num_of_runs)
+            num_of_runs += 1
             print(f"cost:{cost}")
-            print(f"times:{numberOfRuns}")
-            if numberOfRuns >= len(input):
+            print(f"times:{num_of_runs}")
+            if num_of_runs >= len(input):
                 break
 
-        cost = cost/(numberOfRuns*2)
+        cost = cost/(num_of_runs*2)
         return cost
 
     """Quadratic Cost Function"""
-    def cost(self,input, trueLabels, runNumber):
-        outputSqrt = trueLabels[runNumber] - (self.predict(input)[runNumber])
-        return outputSqrt * outputSqrt
+    def cost(self,input, true_labels, run_number):
+        output_sqrt = true_labels[run_number] - (self.predict(input)[run_number])
+        return output_sqrt * output_sqrt
         #y(i) = (0,0,0,0,1,0,0,0,0,0)Transposed
         #x = 28 x 28 = 784 dimensional vector for image
         #C(weights, biases) = sum of (y(i) - prediction(x)) divided 2*number of pixels/ training inputs
--- testBrain.py
+++ testBrain.py
@@ -1,24 +1,24 @@
 #############################################################################################################################################################################################################################################################################################################################################################################################################################################################################
 #
 #       By:Low Yew Qing
-#Objective:To Test the Neural Network That Was Created in neuralnetwork.py and Implementing The User Interface
+# Objective:To Test the Neural Network That Was Created in neuralnetwork.py and Implementing The User Interface
 #    Edits:29/03/2020--Added a way to see accuracy of the network. There's a bug because the prediction is always the same.
 #          01/04/2020--Moved the accuracy tester to neuralnetwork.py based on Sebastian Lague's tutorial.
 #          03/04/2020--Tried testing the cost function. Took to long to run over the training images.
-#
+#          10/09/2020--Made changes in accordance to PEP8
 ##############################################################################################################################################################################################################################################################################################################################################################################################################################################################################
 import neuralnetwork as nn
 import numpy as np
 
 with np.load("mnist.npz") as data:
-    trainingImages = data["training_images"] #50000 images
-    trainingLabels = data["training_labels"]  #50000 labels.
+    training_images = data["training_images"]  # 50000 images
+    training_labels = data["training_labels"]  # 50000 labels.
 
-layerSizes = (784, 5, 10)
+layer_sizes = (784, 5, 10)
 
-brain = nn.ArtificialBrain(layerSizes)
-#print(brain.weights)
-prediction = brain.predict(trainingImages)
-
-print(brain.getCost(trainingImages,trainingLabels))
-brain.printAccuracy(trainingImages,trainingLabels)
+brain = nn.ArtificialBrain(layer_sizes)
+print(brain.weights)
+prediction = brain.predict(training_images)
+print(prediction)
+# print(brain.getCost(training_images,training_labels))
+brain.print_accuracy(training_images, training_labels)
